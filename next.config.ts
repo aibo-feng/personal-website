2882a045b213a0d6de4825e600abe3b580c11a34
Aibo Feng <aibo.feng1@gmail.com>
2025-01-13 06:32:33 -0800

    set basePath and assetPrefix to empty for custom domain

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,6 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = '';
 let basePath = '';
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig: NextConfig = {
   output: 'export', // required for static export
   assetPrefix,      // ensures static assets have the correct prefix
