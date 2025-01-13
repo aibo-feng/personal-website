0bb497fcd542449a4e769d763eb08f63aec32b88
Aibo Feng <aibo.feng1@gmail.com>
2025-01-13 05:32:51 -0800

    changed nextconfig file to support gh pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // required for static export
+  assetPrefix,      // ensures static assets have the correct prefix
+  basePath,         // ensures correct routing on GitHub Pages
+  images: {
+    unoptimized: true, // disables image optimization for static export
+  },
 };
 
 export default nextConfig;
